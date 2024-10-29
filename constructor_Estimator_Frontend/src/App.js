3874fb02965a6e8861a697176ebae96906d9f811
PIERRE H. MUKELA <pierremkl@outlook.com>
2024-10-29 15:51:57 -0500

    Update App.js

--- constructor_Estimator_Frontend/src/App.js
+++ constructor_Estimator_Frontend/src/App.js
@@ -7,7 +7,7 @@ import Estimate from './components/GetEstimateDetail';
 import About from './About';
 import Home from './Home';
 import SignUp from './SignUp';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Register from "./components/Register"
 import SignIn from "./components/SignIn"
 
@@ -16,13 +16,13 @@ function App() {
     <Router> 
       <div className='Estimator'>
         <Nav />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Home}/>
           <Route path="/about" component={About}/>
           <Route path="/signin" component={SignIn}/>
           <Route path="/estimate" component={Estimate}/>
           <Route path="/register" component={Register}/>
-        </Switch>
+        </Routes>
       </div>
 
     </Router>
